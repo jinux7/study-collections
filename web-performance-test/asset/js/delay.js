23a69e85318de69fbda88e8aff4c1463477f44c5
jinux7 <124076788@qq.com>
2019-05-29 10:40:52 +0800

    l

---
+++ web-performance-test/asset/js/delay.js
@@ -0,0 +1,1 @@
+console.log('delay-js文件');
--- web-performance-test/server.js
+++ web-performance-test/server.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var path = require('path');
 app.use('/public', express.static('public'));
 
+// 延迟请求css文件
 app.get('/asset/css/style-asset.css', function (req, res) {
   fs.readFile(path.resolve(__dirname,'asset/css/style-asset.css'), function (err, data) {
     if (err) {
@@ -16,7 +17,21 @@ app.get('/asset/css/style-asset.css', function (req, res) {
     }, 10000);
   });
 })
- 
+
+// 延迟请求js文件
+app.get('/asset/js/delay.js', function (req, res) {
+  fs.readFile(path.resolve(__dirname,'asset/js/delay.js'), function (err, data) {
+    if (err) {
+        return console.error(err);
+    }
+    // console.log("异步读取: " + data.toString());
+    setTimeout(function() {
+      res.header('Content-Type', 'application/javascript; charset=utf-8');
+      res.send(data.toString());
+    }, 5000);
+  });
+})
+
 var server = app.listen(3000, function () {
   var host = server.address().address
   var port = server.address().port
