4a6f26a7b3433035d94c8ea1bbc78a713e64e0e2
jinux <124076788@qq.com>
2023-07-28 16:48:36 +0800

    l

--- SVG/地图/iscroll-zoom版本/assets/util.js
+++ SVG/地图/iscroll-zoom版本/assets/util.js
@@ -18,17 +18,19 @@ function getScaleVal(nWrap) {
 // 通过经纬度转换成百分比
 function transformLocation(arr) {
   // 以北京和广州机场为基准
-  var BASEX_BEIJING = 0.7,
-      BASEY_BEIJING = 0.33,
-      BASEX_GUANGZHOU = 0.65,
-      BASEY_GUANGZHOU = 0.66;
-  var BASELONGITUDE_BEIJING = 116.4240655,
-      BASELATITUDE_BEIJING = 39.5169397,
-      BASELONGITUDE_GUANGZHOU = 113.3094958,
-      BASELATITUDE_GUANGZHOU = 23.39249;
+  var BASEX_BEIJING = 0.698,
+      BASEY_BEIJING = 0.345,
+      BASEX_LASHA = 0.25,
+      BASEY_LASHA = 0.55;
+  var millBeijing = convert(116.4240655, 39.5169397);
+  var millLasha = convert(90.9119034, 29.2978001);
+  var BASELONGITUDE_BEIJING = millBeijing[0],
+      BASELATITUDE_BEIJING = millBeijing[1],
+      BASELONGITUDE_LASHA = millLasha[0],
+      BASELATITUDE_LASHA = millLasha[1];
   return [
-    (BASEX_GUANGZHOU-BASEX_BEIJING)*(arr[0]-BASELONGITUDE_BEIJING)/(BASELONGITUDE_GUANGZHOU-BASELONGITUDE_BEIJING)+BASEX_BEIJING,
-    (BASEY_GUANGZHOU-BASEY_BEIJING)*(arr[1]-BASELATITUDE_BEIJING)/(BASELATITUDE_GUANGZHOU-BASELATITUDE_BEIJING)+BASEY_BEIJING
+    (BASEX_LASHA-BASEX_BEIJING)*(arr[0]-BASELONGITUDE_BEIJING)/(BASELONGITUDE_LASHA-BASELONGITUDE_BEIJING)+BASEX_BEIJING,
+    (BASEY_LASHA-BASEY_BEIJING)*(arr[1]-BASELATITUDE_BEIJING)/(BASELATITUDE_LASHA-BASELATITUDE_BEIJING)+BASEY_BEIJING
   ];
 }
 // 节流
@@ -43,4 +45,19 @@ function throttle(fn, wait) {
           pre = Date.now()
       }
   }
+}
+// 经纬度坐标转平面
+function convert(lon, lat) { // lat：维度，lon：精度
+  var L = 6381372 * Math.PI * 2,//地球周长
+      W = L,// 平面展开后，x轴等于周长 
+      H = L / 2,// y轴约等于周长一半
+      mill = 2.3,// 米勒投影中的一个常数，范围大约在正负2.3之间
+      x = lon * Math.PI / 180,// 将经度从度数转换为弧度
+      y = lat * Math.PI / 180;// 将纬度从度数转换为弧度
+  var temp = Math.tan(0.25 * Math.PI + 0.4 * y);
+  y = 1.25 * Math.log(temp);// 米勒投影的转换
+  // 弧度转为实际距离  
+  x = (W / 2) + (W / (2 * Math.PI)) * x;
+  y = (H / 2) - (H / (2 * mill)) * y;
+  return [x, y];
 }
