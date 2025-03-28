a3a2753c7053d357b953f342e17c0502c9eb339a
jinux <124076788@qq.com>
2025-03-28 14:36:26 +0800

    webassembly

--- webAssembly/src/main.js
+++
@@ -1,6 +0,0 @@
-fetch('../out/main.wasm').then(response =>
-  response.arrayBuffer()
-).then(bytes => WebAssembly.instantiate(bytes)).then(results => {
-  instance = results.instance;
-  document.getElementById("container").textContent = 'assembly->' + instance.exports.main(56789);
-}).catch(console.error);
